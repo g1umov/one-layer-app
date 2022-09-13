bcd41aef59597ff78afdfa717e4048edf8b95150
g1umov <vladislavglumov30@gmail.com>
2022-09-13 10:48:10 +0400

    Setup project

--- CALayerApp/main.m
+++ CALayerApp/main.m
@@ -10,9 +10,10 @@
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
+    
     @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
+    
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
